14f8d0074fac5ee6d13235c2f7f07b56d7006607
Salim A. JOLY <el.salim.salim@gmail.com>
2020-01-27 12:25:46 +0100

    modif select tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
     def am_pm(valeur)
         valeur ? "PM" : "AM"
     end
+
+    def select_option_tag(label, id, model_id)
+        if (id == model_id)
+            content_tag(:option, label, selected: "selected", value: id)
+        else
+            content_tag(:option, label, value: id)
+        end
+    end
 end
