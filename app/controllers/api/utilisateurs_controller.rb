0bec566aa005df17deae2e8d8e936677f8a747e8
Salim A. JOLY <el.salim.salim@gmail.com>
2020-02-18 12:37:39 +0100

    ajustement API

--- app/controllers/api/utilisateurs_controller.rb
+++ app/controllers/api/utilisateurs_controller.rb
@@ -4,7 +4,22 @@ class Api::UtilisateursController < ApiController
     # INDEX #    Renvoi la liste des utilisateurs
     #########
     def index
-        render json: Utilisateur.all
+        groupes = Hash.new
+        utilisateurs = Hash.new
+        Groupe.order(:nom).each do |groupe|
+            unless groupes.has_key?(groupe.id)
+                groupes[groupe.id] = groupe.nom
+            end
+        end
+        Utilisateur.order(:groupe_id, :nom, :prenom).select(:id, :prenom, :nom, :groupe_id).each do |utilisateur|
+            temp = Hash.new
+            temp[:id] = utilisateur.id
+            temp[:nom] = utilisateur.nom
+            temp[:prenom] = utilisateur.prenom
+            temp[:groupe] = groupes[utilisateur.groupe_id]
+            utilisateurs[utilisateur.id] = temp
+        end
+        render json: utilisateurs
     end
 
     ########
