7a24109e0131cb0f251d5ed7cf52613b244b800d
Salim A. JOLY <el.salim.salim@gmail.com>
2020-02-19 15:00:54 +0100

    update API jours et semaine

--- app/controllers/api/semaines_controller.rb
+++ app/controllers/api/semaines_controller.rb
@@ -1,50 +1,96 @@
 class Api::SemainesController < ApiController
+    before_action :find_semaine, only: [:semaine, :jour]
 
     #########
     # INDEX #    Renvoi la liste des objets SEMAINE de la semaine courante
     #########
     def index
         paquet = Hash.new
-        numero_semaine = format_numero_semaine(Date.today.year, Date.today.cweek)
-        semaines = Semaine.where(numero_semaine: numero_semaine)
-        if semaines.count>0 
-            paquet[:numero_semaine] = semaines.first.numero_semaine
-            paquet[:date_lundi] = semaines.first.date_lundi
-        
+        render json: paquet
+    end
+
+    def jour
+        if params.has_key?(:date)
+            jour = params[:date].to_date.cwday
+        else 
+            jour = Date.today.cwday
+        end
+        contenu = contenu_semaine(true, jour)
+        render json: contenu
+    end
+
+    def semaine
+        render json: contenu_semaine
+    end
+
+
+
+    private
+
+    def find_semaine
+        case params[:action]
+        when "semaine"
+            if params.has_key?(:numero_semaine)
+                numero_semaine = params[:numero_semaine]
+            else
+                numero_semaine = format_numero_semaine(Date.today.year, Date.today.cweek)
+            end
+        when "jour"
+            if params.has_key?(:date)
+                date = params[:date].to_date
+                numero_semaine = format_numero_semaine(date.year, date.cweek)
+            else
+                numero_semaine = format_numero_semaine(Date.today.year, Date.today.cweek)
+            end
+        else 
+            numero_semaine = format_numero_semaine(Date.today.year, Date.today.cweek)
+        end
+        @semaines = Semaine.where(numero_semaine: numero_semaine)
+    end
+
+
+    def contenu_semaine(wl=false, num_jour=nil)
+        paquet = Hash.new
+        if @semaines.count>0 
+            paquet[:numero_semaine] = @semaines.first.numero_semaine
+            paquet[:date_lundi] = @semaines.first.date_lundi
+
             semaines_temp = Hash.new
-            semaines.each do |semaine|
+            @semaines.each do |semaine|
                 semaine_temp = Hash.new
                 semaine_temp[:id] = semaine.id
                 semaine_temp[:note] = semaine.note
                 semaine_temp[:utilisateur] = semaine.utilisateur_id
-                
+                paquet[:semaines] = Hash.new
                 jobs_temp = Hash.new
                 semaine.jobs.each do |job|
-                    job_temp = Hash.new
-                    job_temp[:id] = job.id
-                    job_temp[:numero_jour] = job.numero_jour
-                    job_temp[:am_pm] = job.am_pm
-                    job_temp[:note] = job.note
+                    if (num_jour==job.numero_jour)
+                        job_temp = Hash.new
+                        job_temp[:id] = job.id
+                        job_temp[:numero_jour] = job.numero_jour
+                        job_temp[:am_pm] = job.am_pm
+                        job_temp[:note] = job.note
 
-                    wls_temp = Hash.new
-                    job.working_lists.each do |wl|
-                        wl_temp = Hash.new
-                        wl_temp[:id] = wl.id
-                        wl_temp[:code] = wl.work.code
-                        wl_temp[:nom] = wl.work.nom
-                        wls_temp[wl.id] = wl_temp
+                        wls_temp = Hash.new
+                        if wl
+                            job.working_lists.each do |working_list|
+                                wl_temp = Hash.new
+                                wl_temp[:id] = working_list.id
+                                wl_temp[:code] = working_list.work.code
+                                wl_temp[:nom] = working_list.work.nom
+                                wls_temp[working_list.id] = wl_temp
+                            end
+                        end
+                        job_temp[:working_list] = wls_temp
+
+                        jobs_temp[job.numero_jour] = job_temp
                     end
-                    job_temp[:working_list] = wls_temp
-
-                    jobs_temp[job.numero_jour] = job_temp
                 end
                 semaine_temp[:jobs] = jobs_temp
-
                 semaines_temp[semaine.id] = semaine_temp
             end
             paquet[:semaines] = semaines_temp
+            return paquet
         end
-
-        render json: paquet
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,18 @@ Rails.application.routes.draw do
     get 'ping' => 'table_tennis#ping'
     
     resources :semaines, only: [ :show, :index ], param: :identifier
-    #get "utilisateurs/:id/semaine", to: "utilisateurs#semaine"
-    
     resources :utilisateurs, only: :index
+
+    # Détails pour un jour en particulier
+    get "jour", to: "semaines#jour" # Aujourd'hui
+    get "jour/:date", to: "semaines#jour" # Jour spécifié
+
+    # Planning de la semaine (sans détails)
+    get "semaine", to:"semaines#semaine"  # Semaine courante
+    get "semaine/:numero_semaine", to:"semaines#semaine"  # Semaine spécifiée
+
     #get "semaines/:id/utilisateurs", to: "semaines#utilisateurs"
+    #get "utilisateurs/:id/semaine", to: "utilisateurs#semaine"
     
   end
 
