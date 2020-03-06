728c3bf10f0eaecdf7c82f68154397a78b663ce9
Salim A. JOLY <el.salim.salim@gmail.com>
2020-03-06 10:42:39 +0100

    encore un peu

--- app/controllers/ajouts_controller.rb
+++ app/controllers/ajouts_controller.rb
@@ -1,15 +1,39 @@
 class AjoutsController < ApplicationController
 
+    #############
+    #   INDEX   #
+    #############
     def index
-        render json: Ajout.last
+        @ajouts = Ajout.all
+        if @ajouts.count > 1
+            dernier = @ajouts.last
+            @ajouts.each do |ajout|
+                unless ajout == dernier
+                    ajout.destroy
+                    @ajouts.delete(ajout)
+                end
+            end
+        end
+        @works = Work.all
+        @semaines = Semaine.where(date_lundi: Ajout.last.date_lundi).order(:utilisateur_id)
     end
 
+
+    ##########
+    #   NEW  #
+    ##########
     def new
         @ajout = Ajout.new
     end
     
+
+    ##############
+    #   CREATE   #
+    ##############
     def create
         @ajout = Ajout.create(ajout_params)
+        @ajout.numero_jour = @ajout.date_lundi.cwday
+        @ajout.date_lundi = @ajout.date_lundi.beginning_of_week
         if @ajout.save
             flash[:notice] = "ajout créé avec succès"
             redirect_to ajouts_path
@@ -17,8 +41,6 @@ class AjoutsController < ApplicationController
             render :new
         end
     end
-
-
 
     private
 
