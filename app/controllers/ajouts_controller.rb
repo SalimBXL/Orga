f81454e99cb7ed35b15d94b8f5d527c49ca0fa81
Salim A. JOLY <el.salim.salim@gmail.com>
2020-06-03 12:18:21 +0200

    Validation

--- app/controllers/ajouts_controller.rb
+++ app/controllers/ajouts_controller.rb
@@ -94,43 +94,26 @@ class AjoutsController < ApplicationController
         job = nil
 
         ajout_created = false
-        utilisateur = Utilisateur.find(@ajout.utilisateur)
-        numero_semaine = format_numero_semaine(@ajout.date_lundi.year, @ajout.date_lundi.cweek)
-        numero_jour = @ajout.numero_jour
-        slug = "#{utilisateur.prenom_nom} #{numero_semaine}".parameterize
+        utilisateur = Utilisateur.find_by_id(@ajout.utilisateur_id)
+        work1 = Work.find_by_id(@ajout.work1)
+        jour = Jour.where(date: @ajout, utilisateur: @ajout.utilisateur_id, am_pm: @ajout.am_pm, service_id: work1.service_id)
 
-        
-
-        # Semaine
-        # Check si semaine existe. Sinon, on la crée.
-        unless Semaine.exists?(slug: slug)
-            semaine = Semaine.create(numero_semaine: numero_semaine, utilisateur: utilisateur, date_lundi: @ajout.date_lundi)
-            unless semaine.save
+        # Jour
+        # Check si jour existe. Sinon, on le crée.
+        unless Jour.nil?
+            jour = Jour.create(utilisateur: utilisateur, date: @ajout.date, am_pm: @ajout.am_pm, service: work1.service)
+            unless jour.save
                 flash[:danger] = "Impossible de créer la semaine..."
                 redirect_to ajouts_path
             end
-        else
-            semaine = Semaine.find_by_slug(slug)
-        end
-
-        # Job
-        # Check si job existe. Sinon, on le crée.
-        unless Job.exists?(numero_jour: numero_jour, am_pm: @ajout.am_pm, semaine: semaine)
-            job = Job.create(numero_jour: numero_jour, am_pm: @ajout.am_pm, semaine_id: semaine.id)
-            unless job.save
-                flash[:danger] = "Impossible de créer le job..."
-                redirect_to ajouts_path
-            end
-        else
-            job = Job.where(numero_jour: numero_jour, am_pm: @ajout.am_pm, semaine: semaine).first
         end
         
         # Working_list
         # Check si working_list existe. Sinon, on la crée.
         @ajout.works.each do |w|
-            unless WorkingList.exists?(job: job, work: w) && Work.exists?(w)
+            unless WorkingList.exists?(jour: jour, work: w) && Work.exists?(w)
                 work = Work.find(w)
-                working_list = WorkingList.create(job: job, work: work)
+                working_list = WorkingList.create(jour: jour, work: work)
                 unless working_list.save
                     flash[:danger] = "Impossible de créer la working liste suivante : JOB ID:#{job.id} WORK ID:#{w}"
                     redirect_to ajouts_path
