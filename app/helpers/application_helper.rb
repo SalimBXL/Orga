fd916c67acb953b9a5214213c165f6d8d826c436
Salim A. JOLY <el.salim.salim@gmail.com>
2020-12-28 09:24:55 +0100

    ass calender day

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,6 +29,37 @@ module ApplicationHelper
         end
     end
 
+    def nom_mois(numero)
+        case numero
+        when 1
+            nom_jour = I18n.t("Janvier")
+        when 2
+            nom_jour = I18n.t("Fevrier")
+        when 3
+            nom_jour = I18n.t("Mars")
+        when 4
+            nom_jour = I18n.t("Avril")
+        when 5
+            nom_jour = I18n.t("Mai")
+        when 6
+            nom_jour = I18n.t("Juin")
+        when 7
+            nom_jour = I18n.t("Juillet")
+        when 8
+            nom_jour = I18n.t("Aout")
+        when 9
+            nom_jour = I18n.t("Septembre")
+        when 10
+            nom_jour = I18n.t("Octobre")
+        when 11
+            nom_jour = I18n.t("Novembre")
+        when 12
+            nom_jour = I18n.t("Decembre")
+        else
+            nom_jour = nil
+        end
+    end
+
     def formatWorkListe(liste)
         etiquette = ""
         liste.each do |item|
