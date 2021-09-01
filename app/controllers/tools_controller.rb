eabb4fd9388d4b8297d6e1e4bae7a52d6869d29c
Salim A. JOLY <el.salim.salim@gmail.com>
2021-09-01 14:36:32 +0200

    check user

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -110,24 +110,45 @@ class ToolsController < ApplicationController
     ###############
     def check_users
         @problemes = Hash.new
+        emails = Array.new
+        @pas_profil_utilisateur = Array.new
+        @pas_profil_connection = Array.new
+        
 
         # on fait la liste des utilisateurs et des profils de login
         @utilisateurs = Utilisateur.all
-        @profiles = User.all
-
-        # pour chaque utilisateur, on vérifie s'il y a au moins un profil
         if @utilisateurs
             @utilisateurs.each do |utilisateur|
+                emails << utilisateur.email
                 @problemes[utilisateur.email] ||= Hash.new
                 @problemes[utilisateur.email]["utilisateur"] = utilisateur
             end
         end
 
-        # pour chaque profil, on vérifie s'il y a au moins un utilisateur
+        @profiles = User.all
         if @profiles
             @profiles.each do |profile|
+                emails << profile.email
                 @problemes[profile.email] ||= Hash.new
                 @problemes[profile.email]["profile"] = profile
+            end
+        end
+
+        # pour chaque utilisateur, on vérifie s'il y a au moins un profil
+        emails.each do |email|
+            if (@problemes[email]["utilisateur"] and @problemes[email]["profile"])
+                # Ok, on a deux profils : connection et utilisateur
+            else
+                # Problème, il manque un des deux profils...
+
+                if (@problemes[email]["utilisateur"] and @problemes[email]["profile"].nil?)
+                    # On a un profil utilisateur mais pas de connection
+                    @pas_profil_connection << email
+                end
+                if (@problemes[email]["utilisateur"].nil? and @problemes[email]["profile"])
+                    # On a un profil de connection mais pas de profil utilisateur
+                    @pas_profil_utilisateur << email
+                end
             end
         end
     end
